81430eb4caa15be8c16e69583684f9054eea8269
Curt J. Sampson <cjs@cynic.net>
2018-05-05 13:29:58 +0900

    thing_test: Add import tests (now 2 tests failing)
    These demonstrate two things:
    1. That we can import modules via our intended hierarchy. (We had
       already tested that implicitly before with the import, but we now
       make this an explicit test for clarity.)
    2. We do not have an unwanted path in our `sys.path` pointing into
       the internals of our hierarchy that allows us to import those as
       top-level modules. (This test fails.)
    
    demonstrating that we whave

--- project/util/thing_test.py
+++ project/util/thing_test.py
@@ -1,4 +1,4 @@
-import project.util.thing
+from pytest import raises
 
 def test_nothing():
     assert 1
@@ -11,3 +11,13 @@ def test_modulename():
         will see this test pass.
     '''
     assert 'project.util.thing_test' == __name__
+
+def test_good_import():
+    import project.util.thing
+
+def test_bad_import():
+    ''' While we have a `project.util.thing` deep in our hierarchy, we do
+        not have a top-level `thing` module, so this import should fail.
+    '''
+    with raises(ImportError):
+        import thing
